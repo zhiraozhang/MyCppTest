603bc9e71d5390aecd0bcc87579d8cbb0c5bb344
Sherlock <SherlockZhang@aliyun.com>
2022-02-11 17:16:12 +0800

    新增KMP算法

--- Test/KMP.cpp
+++ Test/KMP.cpp
@@ -10,59 +10,73 @@
 #pragma once
 ================================================================*/
 #include <iostream>
-//#include "string.h"
+#include <cstring>
 
 using namespace std;
 
 // 求next数组是不需要主字符串的，只需要匹配字符串
 // 说白了，next[]数组求的是匹配字符串的最大匹配数
-void getNext(const char *str, int *next)
+void getNext(const char *str, int next[])
 {
     int str_len = strlen(str);
-    int pre = 1, post = 1;
-    while (pre < str_len)
+    next[0] = -1;
+    int i = 0, j = -1;
+    while (i < str_len)
     {
-        if (post == -1 || str[pre] == str[post])
-        {
-            pre++;
-            post++;
-            next[pre] = post;
-        } else post = next[post];
-    }
-}
-
-int KMP(const char *mas_str, const char *ano_str)
-{
-    int mas_len = strlen(mas_str), ano_len = strlen(ano_str);
-    if (mas_len < ano_len) return -1;
-    int i = 0, j = 0;
-    int *next = new int[ano_len];
-    getNext(ano_str, next);
-    while (i < mas_len && j < ano_len)
-    {
-        // 匹配：则匹配下一个；直到有一个字符串走完
-        // 不匹配：回退匹配字符串的j指针，回退到匹配字符串当前最长匹配字符串的位置。如果还不匹配则继续回退。
-        if (j == 1 || mas_str[i] == ano_str[j])
+        if (j == -1 || str[i] == str[j])
         {
             i++;
             j++;
+            next[i] = j;
         } else j = next[j];
     }
+    //cout << "next : " << next << endl;
+}
+
+int KMP(const char *s, const char *t)
+{
+    int s_len = strlen(s);
+    int t_len = strlen(t);
+
+    if (s_len < t_len) return -1;//主串长度少于匹配串
+
+    int *next = new int[t_len];
+    getNext(t, next);
+    int i = 0, j = 0;
+    while (i < s_len && j < t_len)
+    {
+        // 匹配：则匹配下一个；直到有一个字符串走完
+        // 不匹配：回退模式串的j指针，回退到模式串当前最长匹配字符串的位置。如果还不匹配则继续回退。
+        if (j == -1 || s[i] == t[j])
+        {
+            i++;
+            j++;
+        } 
+        else
+        {
+            j = next[j];
+        }
+    }
+    //delete[] next;
     //j走完证明匹配上了，返回位置。否则就是失败
-    if (j > ano_len) return i - j;
-    else return -1;
+    if (j >= t_len)
+        return i - j;
+    else
+        return -1;
 }
 
 int main()
 {
-    const char *master_string = "abbaabbaaba";
-    const char *another_string = "abbaaba";
+    char *master_string = "abbaaabbaaba";
+    char *another_string = "abbaaba";
+    cout << "KMP!";
     //cout << "KMP! Start!：" << endl;
     //cin >> master_string >> another_string;
-    int kmp_number = KMP(master_string, another_string);
-    if (kmp_number == -1) cout << "匹配失败" << endl;
-    else cout << "匹配成功，位置：" << kmp_number << endl;
-    delete master_string, another_string;
+    int kmp_return = KMP(master_string, another_string);
+    if (kmp_return == -1) cout << "false" << endl;
+    else cout << "123" << kmp_return << endl;
+    delete master_string;
+    delete another_string;
     system("pause");
     return 0;
 }
