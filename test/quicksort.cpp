918121fd535562a7cf53d2984a2a5f35daf4fdf3
SherlockZhang <SherlockZhang@aliyun.com>
2022-03-02 17:41:31 +0800

    add quicksort.cpp

---
+++ test/quicksort.cpp
@@ -0,0 +1,34 @@
+/*================================================================
+*     Copyright (c) 2022年 SherlockZhang. All rights reserved.
+*
+*   文件名称：quicksort.cpp
+*   创 建 者：SherlockZhang
+*   邮    箱：SherlockZhang@aliyun.com
+*   创建日期：2021年03月02日
+*   描    述：快速排序。
+*
+#pragma once
+================================================================*/
+#include <iostream>
+using namespace std;
+
+void QuickSort(string sort, int left, int right)
+{
+    if (sort == NULL)
+    {
+        return;
+    }
+    if(left<right)
+    {
+
+    }
+}
+
+int main()
+{
+    string *sort_array = "0758346219";
+    cout<<"快速排序前数组sort_array为";
+    //QuickSort(sort_array);
+    cout<<"快速排序后数组sort_array为");
+    return 0;
+}
