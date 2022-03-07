b9ae3cecdf1ca260aa64885a0e2af719d39ba8c7
SherlockZhang <SherlockZhang@aliyun.com>
2022-03-07 12:00:11 +0800

    快排第一版

--- test/quicksort.cpp
+++ test/quicksort.cpp
@@ -10,25 +10,57 @@
 #pragma once
 ================================================================*/
 #include <iostream>
+#include "printarr.h"
 using namespace std;
 
-void QuickSort(string sort, int left, int right)
+void QuickSort(int arr[], int left, int right)
 {
-    if (sort == NULL)
+    if (arr == NULL)
     {
         return;
     }
-    if(left<right)
+    if (left >= right)
     {
-
+        return;
     }
+    int i = left;
+    int j = right;
+    int key = arr[left];
+    while (1)
+    {
+        while (arr[++i] < key)
+        {
+            if (i == right)
+            {
+                break;
+            }
+        }
+        while (arr[--j] > key)
+        {
+            if (j == left)
+            {
+                break;
+            }
+        }
+        if (i >= j)
+        {
+            break;
+        }
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    arr[left] = arr[j];
+    arr[j] = key;
+    QuickSort(arr, left, j - 1);
+    QuickSort(arr, j + 1, right);
 }
 
 int main()
 {
-    string *sort_array = "0758346219";
-    cout<<"快速排序前数组sort_array为";
-    //QuickSort(sort_array);
-    cout<<"快速排序后数组sort_array为");
+    int sort_array[10] = {2, 4, 1, 5, 0, 8, 6, 9, 3, 7};
+    PrintArr(sort_array, "快速排序前数组sort_array为：");
+    QuickSort(sort_array, 0, 9);
+    PrintArr(sort_array, "快速排序后数组sort_array为：");
     return 0;
 }
