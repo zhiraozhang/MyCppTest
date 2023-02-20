4f9aac493177c69bd09f8f2da9977188370c2da0
SherlockZhang <SherlockZhang@aliyun.com>
2023-02-21 00:05:56 +0800

    vector

--- test/泛型和STL-STL/vector.cpp
+++ test/泛型和STL-STL/vector.cpp
@@ -17,7 +17,11 @@ int main()
     {
         cout << "value of vec [" << i << "] = " << vec[i] << endl;
     }
-    // 使用迭代器 iterator 访问值
+    // 三种遍历方式
+    // while
+    // for
+    // for_each
+    //  使用迭代器 iterator 访问值，vec.begin()启示迭代器，指向第一个元素。vec.end()结束迭代器，指向容器中最后一个元素的下一个位置
     vector<size_t>::iterator v = vec.begin();
     while (v != vec.end())
     {
